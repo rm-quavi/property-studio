56066c95292b45b71eab0eb8c7ffad1592d17357
Reymark Arsenio <jarvis@apples-MacBook-Air.local>
2025-07-25 22:36:48 +0800

    remove dev tools

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools(), tailwindcss()],
+  plugins: [vue(), tailwindcss()],
   test: {
     environment: 'jsdom',
     globals: true,
